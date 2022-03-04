5d9ea1d85395d17ff89fb9b920cb7348e0d42b28
DESKTOP-QKHPRFT\style <styleyo12@naver.com>
2022-03-04 13:26:37 +0900

    크롭 앱 만들기 - 2

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
+
+function onLoginSubmit() {
     const username = loginInput.value;
     console.log(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginButton.addEventListener("submit", onLoginSubmit);
