c9f4c0553e1d0694050348dd5ba3d3f5c957a2e7
YKS <styleyo12@naver.com>
2022-03-09 15:03:53 +0900

    크롬 앱 만들기 - 6

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,36 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const toDos = [];
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newToDo);
+    saveToDos();
+    paintTodo(newToDo);
+}
 
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
+
+function deleteToDo(event) {   
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
