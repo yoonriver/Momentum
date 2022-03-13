fb3aa394bcb4bb1e80e8a7a65f76e2d379af26a5
YKS <styleyo12@naver.com>
2022-03-13 12:16:37 +0900

    크롭 앱 만들기 - 8

--- js/todo.js
+++ js/todo.js
@@ -2,22 +2,37 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
-const toDos = [];
+let toDos = [];
 const TODOS_KEY = "todos";
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintTodo);
+}
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newToDo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
     saveToDos();
-    paintTodo(newToDo);
+    paintTodo(newTodoObj);
 }
 
 function paintTodo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteToDo);
@@ -32,14 +47,5 @@ function deleteToDo(event) {
 }
 
 function saveToDos() {
-    localStorage.setItem("TODOS_KEY", JSON.stringify(toDos));
-}
-
-toDoForm.addEventListener("submit", handleToDoSubmit);
-
-const savedToDos = localStorage.getItem(TODOS_KEY);
-
-if (savedToDos !== null) {
-  const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
